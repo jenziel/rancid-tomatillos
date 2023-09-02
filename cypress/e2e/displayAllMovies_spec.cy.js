d3b8c354dca21243d3a07a68478df3e062b99a6f
Avery Berryman <averyberryman@gmail.com>
2023-09-02 14:47:18 -0600

    Add working test for all homepage images with hooks

--- cypress/e2e/displayAllMovies_spec.cy.js
+++ cypress/e2e/displayAllMovies_spec.cy.js
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import mockMoviesData from "../fixtures/movieData.json";
+
+describe("Display All Movies", function () {
+  beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      mockMoviesData
+    ).as("fetchMovies");
+  });
+
+  it("should display all movies when visiting the app", function () {
+    cy.visit("http://localhost:3000");
+    cy.wait("@fetchMovies");
+    cy.contains("Loading...").should("not.exist");
+    cy.get(".movies-container").should("be.visible");
+    mockMoviesData.movies.forEach((movie) => {
+      cy.get('.movies-container img[src="' + movie.poster_path + '"]').should(
+        "be.visible"
+      );
+    });
+  });
+});
