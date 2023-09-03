6ba8c4ba545f1c815908fdc995452ef9e7f13ca7
Avery Berryman <averyberryman@gmail.com>
2023-09-03 14:22:00 -0600

    Store custom commands in the commands.js file so tests are more succinct and readable

--- cypress/e2e/errorHandling_spec.cy.js
+++ cypress/e2e/errorHandling_spec.cy.js
@@ -1,24 +1,30 @@
-describe('Error handling', () => {
-  it('Should handle a 500 error', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', (req) => {
-      req.reply({
+describe("Error handling", () => {
+  it("Should handle a 500 error", () => {
+    cy.interceptMovies();
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270",
+      {
         statusCode: 500,
         body: {
-          fixture: 'errorData.json', 
+          fixture: "errorData.json",
         },
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-      });
-    }).as('getMovieById');
-    cy.visit("http://localhost:3000/")
+      }
+    ).as("getMovieById");
+
+    cy.visit("http://localhost:3000/");
+    cy.wait("@fetchMovies");
     cy.get(".movies-container img.original-view").first().click();
-    cy.contains('Error 500: Internal Server Error').should('be.visible');
+    cy.contains("Error 500: Internal Server Error").should("be.visible");
   });
-  it('Should handle a 404 error', () => {
-    cy.visit('http://localhost:3000/potato');
-    cy.contains(`Woops!`).should('be.visible');
-    cy.contains(`That Page Doesn't Exist`).should('be.visible');
-    cy.get('button').should("contain", 'Return to Homepage')
-  })
+
+  it("Should handle a 404 error", () => {
+    cy.visit("http://localhost:3000/potato");
+    cy.contains(`Woops!`).should("be.visible");
+    cy.contains(`That Page Doesn't Exist`).should("be.visible");
+    cy.get("button").should("contain", "Return to Homepage");
+  });
 });
