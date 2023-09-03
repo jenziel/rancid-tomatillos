07c5f103384f28ec98b76427c7b9bec644e3995e
Jen Ziel <jenziel400@gmail.com>
2023-09-03 14:07:45 -0400

    Style the Return to Home button.  Add hover effect and change positioning.  Change positioning of the movie poster and the title so that they overlap with backdrop image.

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,39 +1,34 @@
 import { useParams, NavLink } from "react-router-dom";
 import "./MovieDetails.css";
-import { useState, useEffect } from "react";
-import Header from '../Header/Header'
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 function MovieDetails(props) {
-  const { movie } = props
-  console.log('props', props)
-  const {id} = useParams()
+  const { movie } = props;
+  console.log("props", props);
+  const { id } = useParams();
   const handleReset = () => {
-    props.resetError()
-    props.resetLoading()
+    props.resetError();
+    props.resetLoading();
   };
-  
-  console.log('movie deconstructed', movie)
 
-  if (!movie ) {
+  console.log("movie deconstructed", movie);
+
+  if (!movie) {
     return <div>Loading movie details...</div>;
   }
-  
+
   return (
-   
-    <div className="unique-movie-page">
+    <div className='unique-movie-page'>
       <main className='App'>
         <div className='unique-movie'>
-          <div className='unique-movie'>
-                 
-      <NavLink to='/' className='nav'>
-      <div onClick={handleReset} className='back-button'>
-          <button>
-            <p>← Return to Home</p>
-          </button>
-      </div>
-      </NavLink>
+          <div className='hero-image'>
+            <NavLink to='/' className='nav'>
+              <div onClick={handleReset} className='back-button'>
+                <button>
+                  <p>← Return to Home</p>
+                </button>
+              </div>
+            </NavLink>
 
             <div className='vignette-container'>
               <img
@@ -43,25 +38,25 @@ function MovieDetails(props) {
               />
               <div className='vignette-overlay'></div>
             </div>
-            <div className='lower-section'>
-              <div className='left-side'>
-                <img
-                  src={movie.poster_path}
-                  alt={movie.title}
-                  className='movie-poster'
-                />
-              </div>
-              <div className='right-side'>
-                <h2>{movie.title}</h2>
-                <p className='tagline'>{movie.tagline}</p>
-                <p className='overview'>{movie.overview}</p>
-                <p>{movie.average_rating}/10 stars</p>
-                <p>Released: {movie.release_date}</p>
-                <p className='budget'>Budget: ${movie.budget} million</p>
-                <p className='genres'>Genres: {movie.genres}</p>
-                <p className='revenue'>Gross Revenue: ${movie.revenue}</p>
-                <p className='runtime'>Runtime: {movie.runtime} minutes</p>
-              </div>
+          </div>
+          <div className='lower-section'>
+            <div className='left-side'>
+              <img
+                src={movie.poster_path}
+                alt={movie.title}
+                className='movie-poster'
+              />
+            </div>
+            <div className='right-side'>
+              <h2>{movie.title}</h2>
+              <p className='tagline'>{movie.tagline}</p>
+              <p className='overview'>{movie.overview}</p>
+              <p>{movie.average_rating}/10 stars</p>
+              <p>Released: {movie.release_date}</p>
+              <p className='budget'>Budget: ${movie.budget} million</p>
+              <p className='genres'>Genres: {movie.genres}</p>
+              <p className='revenue'>Gross Revenue: ${movie.revenue}</p>
+              <p className='runtime'>Runtime: {movie.runtime} minutes</p>
             </div>
           </div>
         </div>
@@ -84,8 +79,7 @@ MovieDetails.propTypes = {
       budget: PropTypes.number,
       genres: PropTypes.arrayOf(PropTypes.string),
       revenue: PropTypes.number,
-      runtime: PropTypes.number
-    })
-  })
+      runtime: PropTypes.number,
+    }),
+  }),
 };
-
