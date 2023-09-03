ea111f8a2b48d275d83490a573158310c5319ff2
Avery Berryman <averyberryman@gmail.com>
2023-09-03 15:45:59 -0600

    Add sad path for non existent movie

--- cypress/e2e/movieDetails_spec.cy.js
+++ cypress/e2e/movieDetails_spec.cy.js
@@ -7,4 +7,9 @@ describe("Movie Details", function () {
   it("should display movie details when a movie is clicked", function () {
     cy.checkMovieDetails();
   });
+  it("Should handle a non-existent movie", () => {
+    cy.visit("http://localhost:3000/movies/999999");
+    cy.contains(`That Page Doesn't Exist.`).should('be.visible');
+});
+
 });
